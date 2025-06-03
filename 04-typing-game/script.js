5ab69a477ea5398fde19c9fcef76c4371f17708d
Joshua <joxuecont@gmail.com>
2025-06-03 10:23:41 -0400

    add new project: initial commit and basic structure

---
+++ 04-typing-game/script.js
@@ -0,0 +1,13 @@
+const $time = document.querySelector('time');
+const $paragraph = document.querySelector('p');
+const $input = document.querySelector('input');
+
+const INITIAL_TIME = 30;
+
+const TEXT = 'lorem ipsum dolor sit amet person typing some keys';
+
+let words = [];
+let currentTime = INITIAL_TIME;
+
+function initGame() {}
+function initEvents() {}
